30a5623fac3453eb99963d39847d7cc8b2da5a6e
Gabriel dos Santos <gabriel@dc3.com.br>
2015-02-19 15:12:03 -0200

    teste

--- putz/src/main/java/com/dc3/controller/LoginController.java
+++ putz/src/main/java/com/dc3/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.dc3.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,19 +14,20 @@ import com.dc3.service.exception.ServiceException;
 
 @RestController
 @RequestMapping(value = "/login")
-public class LoginController {
+public class LoginController extends GenericController {
 
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(method = RequestMethod.GET, headers = "Accept=application/json")
-	public ResponseEntity<?> validate(User user) {
+	@RequestMapping(value = "validate",method = RequestMethod.POST, headers = "Accept=application/json")
+	public ResponseEntity<?> validate(@RequestBody User user) {
 		try {
 			user = userService.doLogin(user);
-		} catch (ServiceException e) {			
-			return new ResponseEntity<String>(e.getMsgResource(), HttpStatus.UNAUTHORIZED);
-		}
+		} catch (ServiceException e) {
+			return new ResponseEntity<String>(getMessageSource().getMessage(
+					e.getMsgResource(), null, null), HttpStatus.UNAUTHORIZED);
+		}		
 		ResponseEntity<User> respEnt = new ResponseEntity<User>(user,HttpStatus.OK);
-		return respEnt;
+		return respEnt;	
 	}
 }
